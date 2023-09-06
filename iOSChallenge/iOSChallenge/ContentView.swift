29baea9a9bba53f52942d6c45c8069c00632e68c
Sophia Liu <91706701+s0phialiu@users.noreply.github.com>
2023-09-06 15:27:09 -0500

    Added starter code for iOS coding challenge.

---
+++ iOSChallenge/iOSChallenge/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  iOSChallenge
+//
+//  Created by HackIllinois on 9/6/23.
+//
+
+import SwiftUI
+
+// Create the event struct
+struct MyEvent: Codable {
+    let id: String
+    // ... Complete the rest of the struct
+}
+
+// Make your API call to the HackIllinois API event endpoint
+// For help getting started, google "how to make an API call with Swift"
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            // Create your desired sub fields
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSChallenge/iOSChallenge/iOSChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSChallengeApp.swift
+//  iOSChallenge
+//
+//  Created by HackIllinois on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSChallengeApp: App {
+    var body: some Scene {
+        WindowGroup { // Load your view(s) in
+            ContentView()
+        }
+    }
+}
