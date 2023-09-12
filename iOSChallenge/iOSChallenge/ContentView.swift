bb866b9f6faac3cb164a9360cdedfc4817354ffe
Naomi Lin <naomilin.tw@gmail.com>
2023-09-12 18:27:00 -0500

    Organized by time blocks

--- iOSChallenge/iOSChallenge/ContentView.swift
+++ iOSChallenge/iOSChallenge/ContentView.swift
@@ -5,141 +5,6 @@
 //  Created by HackIllinois on 9/6/23.
 //
 
-//import SwiftUI
-//
-//// Create the event struct
-//struct MyEvent: Codable, Identifiable {
-//    let id: String
-//    let name: String
-//    let description: String
-//    let startTime: Int64
-//    let endTime: Int64
-//}
-//
-//struct MyEvents: Codable {
-//    var events : [MyEvent]
-//}
-//// Make your API call to the HackIllinois API event endpoint
-//// For help getting started, google "how to make an API call with Swift"
-//
-////from ChatGPT
-//func fetchDataFromAPI(completion:@escaping (MyEvents) -> ()) {
-//    if let url = URL(string: "https://adonix.hackillinois.org/event/") {
-//        URLSession.shared.dataTask(with: url) { data, response, error in
-//            if let data = data {
-//                do {
-//                    let decoder = JSONDecoder()
-//                    var events = try decoder.decode(MyEvents.self, from: data)
-//
-//                    // from https://stackoverflow.com/questions/26719744/swift-sort-array-of-objects-alphabetically
-//                    events.events.sort { $0.name.localizedStandardCompare($1.name) == .orderedAscending }
-//                    events.events.sort { $0.startTime < $1.startTime }
-//
-//                    DispatchQueue.main.async {
-//                        completion(events)
-//                    }
-//                } catch {
-//                    print("Error decoding JSON: \(error)")
-//                }
-//            } else if let error = error {
-//                print("Error fetching data: \(error)")
-//            }
-//        }.resume()
-//    }
-//}
-//
-//
-//struct ContentView: View {
-//    @State var events: [MyEvent] = []
-//
-//    var body: some View {
-//        VStack {
-//            Text("SCHEDULE")
-//                .font(.custom("Helvetica Neue", size: 50, relativeTo: .body))
-//                .fontWeight(.heavy)
-//                .italic()
-//                .underline()
-//                .foregroundColor(.blue)
-//                .padding(9)
-//            if events.isEmpty {
-//                Text("Loading...")
-//            } else {
-//                List(events) { event in
-//                    VStack {
-//                        TimeOfEvent(integer: event.startTime)
-//                        NameOfEvent(text: event.name)
-//                        DescriptionOfEvent(text: event.description)
-//                    }
-//                    .padding()
-//                    .background(Color.white)
-//                    .cornerRadius(10)
-//                    .shadow(radius: 5)
-//                }
-//            }
-//        }
-//        .padding()
-//        /* from https://medium.com/swift-productions/fetch-json-data-display-list-swiftui-2-0-d301f401c223 */
-//        .onAppear {
-//            fetchDataFromAPI { myEvents in
-//                self.events = myEvents.events
-//            }
-//        }
-//    }
-//}
-//
-//
-//
-//
-//struct NameOfEvent: View {
-//    let text: String
-//
-//    var body: some View {
-//        Text(text)
-//            .bold().foregroundColor(Color.init(UIColor.darkGray))
-//            .font(.system(size: 25))
-//            .frame(maxWidth: .infinity, alignment: .center)
-//            .multilineTextAlignment(.center)
-//            .padding(6)
-//    }
-//}
-//
-//struct DescriptionOfEvent: View {
-//    let text: String
-//    var body: some View {
-//        Text(text)
-//            .font(.system(size: 20)).foregroundColor(.gray)
-//            .frame(maxWidth: .infinity, alignment: .center)
-//            .multilineTextAlignment(.center)
-//            .padding(.bottom, 15)
-//    }
-//}
-//
-//struct TimeOfEvent: View {
-//    let integer: Int64
-//    var body: some View {
-//        Text("\(formattedStartTime)")
-//            .font(.system(size: 22)).foregroundColor(Color.blue).bold()
-//            .frame(maxWidth: .infinity, alignment: .center)
-//            .multilineTextAlignment(.center)
-//            .padding(6)
-//            .overlay(Rectangle().frame(width: nil, height: 2, alignment: .top).foregroundColor(Color.yellow), alignment: .bottom)
-//    }
-//
-//    private var formattedStartTime: String {
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat = "MMM d - hh:mm a"
-//        let date = Date(timeIntervalSince1970: TimeInterval(integer))
-//        return dateFormatter.string(from: date)
-//        }
-//}
-//
-//
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
 
 import SwiftUI
 
@@ -149,51 +14,50 @@ struct MyEvent: Codable, Identifiable {
     let name: String
     let description: String
     let startTime: Int64
-    let endTime: Int64
 }
 
 struct MyEvents: Codable {
-    var events: [MyEvent]
+    var events: [MyEvent] //array events with MyEvent objects
 }
 
 struct ContentView: View {
     @State var events: [MyEvent] = []
     
-    // Helper function to group events by time
+    // helper function to group events by time
     private func groupEventsByTime(events: [MyEvent]) -> [String: [MyEvent]] {
         var groupedEvents: [String: [MyEvent]] = [:]
+        //https://swiftyplace.com/swift-date-formatting-10-steps-guide/#:~:text=%E2%80%9Cyyyy%E2%80%9D%3A%20Represents%20the%20four,in%20a%2024%2Dhour%20format.
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MMM d - HH:mm"
         
         for event in events {
             let formattedTime = dateFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(event.startTime)))
             
-            if var group = groupedEvents[formattedTime] {
-                group.append(event)
+            if var group = groupedEvents[formattedTime] { //checking if time is already grouped; if yes, then append. if not, then
+                group.append(event)                       //create new group
                 groupedEvents[formattedTime] = group
             } else {
                 groupedEvents[formattedTime] = [event]
             }
         }
-        
         return groupedEvents
     }
 
     // Make your API call to the HackIllinois API event endpoint
+    //from ChatGPT
     func fetchDataFromAPI(completion: @escaping (MyEvents) -> ()) {
         if let url = URL(string: "https://adonix.hackillinois.org/event/") {
             URLSession.shared.dataTask(with: url) { data, response, error in
                 if let data = data {
                     do {
                         let decoder = JSONDecoder()
-                        var events = try decoder.decode(MyEvents.self, from: data)
+                        var events = try decoder.decode(MyEvents.self, from: data) //organizing the code
                         
                         // Sort events by name and start time
                         events.events.sort { $0.name.localizedStandardCompare($1.name) == .orderedAscending }
-                        events.events.sort { $0.startTime < $1.startTime }
                        
                         DispatchQueue.main.async {
-                            completion(events)
+                            completion(events) //tells computer the sorting has finished
                         }
                     } catch {
                         print("Error decoding JSON: \(error)")
@@ -217,6 +81,7 @@ struct ContentView: View {
                 })
             }
             .navigationBarTitle("Schedule")
+            /* from https://medium.com/swift-productions/fetch-json-data-display-list-swiftui-2-0-d301f401c223 */
             .onAppear {
                 fetchDataFromAPI { myEvents in
                     self.events = myEvents.events
